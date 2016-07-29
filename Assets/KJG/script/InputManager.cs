1e886d52b206bbcbc8e026b8c94c619012656a2e
griddummy <griddummy@gmail.com>
2016-07-29 22:00:13 +0900

    종권 0729

--- Assets/KJG/script/InputManager.cs
+++ Assets/KJG/script/InputManager.cs
@@ -1,6 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : ScriptableObject {
     
+    public enum Action { FORWARD, BACKWARD, LEFT, RIGHT, JUMP, INVENTORY }
+    public enum Event { DOWN, UP, STAY }
+    public string test = "test";
+
+    private Dictionary<Action, KeyCode> listActionKey;
+
+    private static InputManager m_instance;
+    public static InputManager intstance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                Debug.Log("Create InputManager");
+                m_instance = CreateInstance<InputManager>();
+            }            
+            return m_instance;
+        }        
+    }
+
+    public bool GetKeyState(Action _action, Event _event)// 액션에 해당하는 키의 상태를 알아온다.
+    {
+        switch (_event)
+        {
+            case Event.DOWN:
+                return Input.GetKeyDown(listActionKey[_action]);
+                
+            case Event.STAY:
+                return Input.GetKey(listActionKey[_action]);
+
+            case Event.UP:
+                return Input.GetKeyUp(listActionKey[_action]);
+
+            default:
+                return false;
+        }
+    }
+    void OnEnable()
+    {
+        listActionKey = new Dictionary<Action, KeyCode>();
+    }
 }
---
+++ Assets/KJG/script/Test/MyTestScriptable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTestScriptable : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log("초기 : " + InputManager.intstance.test);
+        InputManager.intstance.test = "ss";
+        Debug.Log("변경 : " + InputManager.intstance.test);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
